378dfe1396bfe927380e606399307929165f3cdf
Vanshaj Garg <vanshaj.garg@bangmetric.com>
2021-07-15 02:12:16 +0530

    self detail

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import add_user, Login, Logout
+from .views import add_user, Login, Logout, Self_detail
 
 
 router = DefaultRouter()
@@ -14,7 +14,7 @@ urlpatterns = [
     # path('reset-password/',VerifyEmail.as_view(), name='reset-password'),
     # path('forget-password-email/',reset_password_email.as_view(), name='reset_password-email'),
     # path('forget-password/',ResetPasswordView.as_view(), name='forget-password'),
-    # path('self-detail/',Self_detail.as_view(), name='self-detail'),
+    path('self-detail/',Self_detail.as_view(), name='self-detail'),
     # path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
     # path('api/resend_verify_email/', resend_verify_email.as_view(), name='resend_verify_email'),
 ]
