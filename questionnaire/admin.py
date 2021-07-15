beafc171fb170dab204fbf690a993da90f1e0ed9
Vanshaj Garg <vanshaj.garg@bangmetric.com>
2021-07-15 14:10:35 +0530

    question model commit

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import framework, category, question_model
 # Register your models here.
+
+admin.site.register(framework)
+admin.site.register(category)
+admin.site.register(question_model)
